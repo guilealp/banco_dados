988ddd46a7711772fd455123df03b6015c352a81
guilealp <guilherme.3110@hotmail.com>
2024-03-14 13:10:03 -0300

    aula 14/03

--- Script-escola.sql
+++ Script-escola.sql
@@ -1,3 +1,5 @@
+drop database if exists escola;
+
 create database escola;
 
 use escola;
@@ -23,7 +25,7 @@ create table disciplinas(
 	id int not null auto_increment,
 	nome varchar(45) not null,
 	numero_identificador varchar(45)not null unique,
-	professor_id int not null,
+	professor_id int null,
 	primary key (id),
 	constraint fk_professor_disciplina
 		foreign key (professor_id)
@@ -82,11 +84,91 @@ INSERT INTO professor  (nome, numero_identificador, especialidade,grau_academico
 
 select * from professor p ;
 
-INSERT INTO disciplinas (nome, numero_identificador, data_nacimento,endereco) VALUES ('aluno 1', '1', '2000-03-04','rua mario costa');
-INSERT INTO disciplinas  (nome, numero_identificador, data_nacimento,endereco) VALUES ('aluno 2', '2', '2000-03-04','rua mario costa');
-INSERT INTO disciplinas  (nome, numero_identificador, data_nacimento,endereco) VALUES ('aluno 3', '3', '2001-03-04','rua mario costa');
-INSERT INTO disciplinas  (nome, numero_identificador, data_nacimento,endereco) VALUES ('aluno 4', '4', '2001-03-04','rua mario costa');
-INSERT INTO disciplinas  (nome, numero_identificador, data_nacimento,endereco) VALUES ('aluno 5', '5', '2002-03-04','rua mario costa');
+INSERT INTO disciplinas (nome, numero_identificador,professor_id) VALUES ('fisica 1', '1','1');
+INSERT INTO disciplinas  (nome, numero_identificador,professor_id) VALUES ('fisica 2', '2','1');
+INSERT INTO disciplinas  (nome, numero_identificador,professor_id) VALUES ('portugues 1', '3','3');
+INSERT INTO disciplinas  (nome, numero_identificador,professor_id) VALUES ('matematica 1', '4','2');
+INSERT INTO disciplinas  (nome, numero_identificador,professor_id) VALUES ('matematica 2', '5','2');
+
+select * from disciplinas d;
+
+
+
+insert into alunos_por_disciplinas (alunos_id,disciplinas_id)
+select a.id as aluno_id ,d.id as disciplinas_id from alunos a cross join  disciplinas d;
+
+
+select *from alunos_por_disciplinas ;
+
+select a.id, a.nome, d.nome  from alunos a 
+inner join alunos_por_disciplinas ad on a.id = ad.alunos_id
+inner join disciplinas d on d.id = ad.disciplinas_id
+where d.nome = 'matematica 1';
+
+
+select a.id, a.nome, d.nome  from alunos a 
+inner join alunos_por_disciplinas ad on a.id = ad.alunos_id
+inner join disciplinas d on d.id = ad.disciplinas_id
+where d.nome = 'matematica 2';
+
+select a.id, a.nome, d.nome  from alunos a 
+inner join alunos_por_disciplinas ad on a.id = ad.alunos_id
+inner join disciplinas d on d.id = ad.disciplinas_id
+where d.nome = 'fisica 1';
+
+select a.id, a.nome, d.nome  from alunos a 
+inner join alunos_por_disciplinas ad on a.id = ad.alunos_id
+inner join disciplinas d on d.id = ad.disciplinas_id
+where d.nome = 'fisica 2';
+
+select a.id, a.nome, d.nome  from alunos a 
+inner join alunos_por_disciplinas ad on a.id = ad.alunos_id
+inner join disciplinas d on d.id = ad.disciplinas_id
+where d.nome = 'portugues 1';
+
+insert into  notas (nota,alunos_id,disciplinas_id) values
+(9.00,1,1),
+(7.25,2,1),
+(9.55,3,1),
+(10.00,4,1),
+(5.00,5,1),
+(6.50,6,1),
+(8.00,7,1),
+(7.00,8,1),
+(8.00,9,1),
+(3.50,10,1),
+(9.50,11,1),
+(10.00,12,1),
+(9.00,13,1),
+(8.00,14,1),
+(7.00,15,1),
+(9.00,16,1),
+(4.00,17,1),
+(1.00,18,1),
+(10.00,19,1),
+(6.00,20,1);
+
+select n.nota, a.nome, d.nome, d.id  from notas n  
+inner join alunos a on a.id = n.alunos_id 
+inner join disciplinas d on d.id = n.disciplinas_id
+where n.nota >= 7;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
